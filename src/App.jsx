62566d55333836b1f639be22b94cebea19b1af2c
MartaFagundez <55920614+MartaFagundez@users.noreply.github.com>
2025-03-17 12:19:31 -0300

    refactor: organize and style component list

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,28 @@ import ProfileCard from "./components/cards/ProfileCard";
 
 function App() {
   return (
-    <div>
-      <div className="flex flex-col justify-center items-center gap-4 min-h-screen bg-gray-100">
+    <div className="flex flex-col justify-center items-center gap-16 min-h-screen bg-gray-100 py-12 px-4">
+      <div>
+        <p className="mb-2 text-2xl text-center text-gray-900 md:text-4xl uppercase">
+          React Patterns
+        </p>
+        <h1 className="mb-4 text-5xl font-extrabold text-center text-gray-900 md:text-6xl">
+          Compound Components
+        </h1>
+      </div>
+
+      {/* CARDS */}
+      <section className="flex flex-col justify-center items-center gap-8">
+        <h2 className="mb-2 text-2xl font-bold text-center text-gray-900 md:text-3xl uppercase">
+          Cards
+        </h2>
         <ProfileCard
-          name="Jane Doe"
-          bio="Desarrolladora web y diseñadora apasionada por la tecnología y el arte."
+          name="Jhon Doe"
+          bio="Web developer and designer passionate about technology and art."
           avatar="https://i.pravatar.cc/150?img=3"
           social={[
-            { platform: "Twitter", url: "https://twitter.com/janedoe" },
-            { platform: "LinkedIn", url: "https://linkedin.com/in/janedoe" },
+            { platform: "Twitter", url: "https://twitter.com/jhondoe" },
+            { platform: "LinkedIn", url: "https://linkedin.com/in/jhondoe" },
           ]}
         />
 
@@ -21,7 +34,7 @@ function App() {
           description="High-quality wireless headphones with noise cancellation and long battery life."
           price="129.99"
         />
-      </div>
+      </section>
     </div>
   );
 }
